9be80ec19cf4eb7761704980d9f6e58d2b0f4dd9
ADH <adadhill@gmail.com>
2016-02-15 07:10:33 -0800

    Python script to pull down diffs from Fed Reg

---
+++ Reg.Frequency copy.py
@@ -0,0 +1,24 @@
+#Dot delimited, which separates Parts and Sections. 
+#Delete the "." in re.split in order to get point citations on highly altered sections
+
+import os, re, sys
+os.chdir('/Users/getadh/Desktop/Title') #Change where relevant 
+
+label = "Title50.txt"
+contents = open(label, "r")
+string = contents.read()
+def text(label):
+     file = open(label, "r")
+     string = file.read()
+     text = re.split(r"(\s+|[,:;.\"()/&]|[?!]+|-{2,})",string)
+     return text
+
+from collections import Counter 
+Counter(text(label))
+a = Counter(text(label))
+
+#Print to text file, where "a" is a = Counter(inputText(filename))
+
+#output = open('Title50.out.txt', 'w')
+#output.write(str(a))
+#output.close()
